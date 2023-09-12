20e9d64262fa3ba72031baa5c59de7d45fb71349
Stanimir Stoychev <s.stoychev@smartbroker.de>
2023-09-12 11:25:41 +0300

    Next-CMS: add tooltips to dashboard toolbar

--- apps/next-cms/src/layout/dashboard/DashboardToolbar.tsx
+++ apps/next-cms/src/layout/dashboard/DashboardToolbar.tsx
@@ -7,12 +7,14 @@ import { AwesomeIcon, Menu } from '~/components';
 function ToolbarLink({
     className,
     iconProps,
+    tooltip,
     ...rest
 }: Omit<React.ComponentProps<typeof NextLink>, 'children'> & {
     iconProps: React.ComponentProps<typeof AwesomeIcon>;
+    tooltip?: React.ReactNode;
 }) {
     return (
-        <NextLink className={clsx('text-center group', className)} {...rest}>
+        <NextLink className={clsx('text-center group', className, tooltip && 'has-tooltip')} {...rest}>
             <AwesomeIcon
                 size="2x"
                 {...iconProps}
@@ -21,7 +23,44 @@ function ToolbarLink({
                     iconProps.className,
                 )}
             />
+            {tooltip && (
+                <div className="px-1 text-black -translate-y-1/2 rounded shadow-lg pointer-events-none top-1/2 bg-primary left-[120%] min-w-max group-hover:pointer-events-auto tooltip">
+                    {tooltip}
+                </div>
+            )}
         </NextLink>
+    );
+}
+
+function UserMenu() {
+    const { data } = useSession();
+
+    return (
+        <Menu
+            data-testid="user-menu"
+            position="top-right"
+            className="text-center"
+            btnProps={{
+                className:
+                    'group has-tooltip hover:scale-110 transition-all hover:shadow-lg active:scale-110 active:shadow-lg focus:scale-110 focus:shadow-lg',
+            }}
+            options={[
+                { children: <NextLink href="/">Public website</NextLink> },
+                {
+                    onClick: () => signOut(),
+                    children: 'Sign out',
+                },
+            ]}
+        >
+            {data?.user?.image ? (
+                <img src={data.user.image} alt="User avatar" className="w-8 rounded" />
+            ) : (
+                <AwesomeIcon icon="user" />
+            )}
+            <span className="top-1/2 -translate-y-1/2 p-1 text-black rounded shadow-lg pointer-events-none bg-primary left-[120%] min-w-max group-hover:pointer-events-auto tooltip">
+                {data?.user?.name}
+            </span>
+        </Menu>
     );
 }
 
@@ -33,33 +72,11 @@ export function DashboardToolbar() {
             className="flex flex-col gap-2 px-3 py-2 shadow-md bg-secondary rounded-e text-primary"
             data-testid="dashboard-header"
         >
-            <ToolbarLink href="/dashboard" iconProps={{ icon: 'poo' }} />
+            <ToolbarLink href="/dashboard" iconProps={{ icon: 'poo' }} tooltip="Dashboard" />
             <hr className="my-4" />
-            <ToolbarLink href="/dashboard" iconProps={{ icon: 'cubes-stacked', size: 'lg' }} />
-            <ToolbarLink href="/dashboard/pages" iconProps={{ icon: 'book-bookmark', size: 'lg' }} />
+            <ToolbarLink href="/dashboard/pages" iconProps={{ icon: 'book-bookmark', size: 'lg' }} tooltip="Pages" />
             <div className="flex-1" />
-            <Menu
-                data-testid="user-menu"
-                position="top-right"
-                className="text-center"
-                btnProps={{
-                    className:
-                        'hover:scale-110 transition-all hover:shadow-lg active:scale-110 active:shadow-lg focus:scale-110 focus:shadow-lg',
-                }}
-                options={[
-                    { children: <NextLink href="/">Public website</NextLink> },
-                    {
-                        onClick: () => signOut(),
-                        children: 'Sign out',
-                    },
-                ]}
-            >
-                {data?.user?.image ? (
-                    <img src={data.user.image} alt="User avatar" className="w-8 rounded" />
-                ) : (
-                    <AwesomeIcon icon="user" />
-                )}
-            </Menu>
+            <UserMenu />
         </header>
     );
 }
