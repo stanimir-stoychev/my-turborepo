a3f71cc663aa32cebb834464429cc3d450f3c4ab
Stanimir Stoychev <s.stoychev@smartbroker.de>
2023-10-08 08:36:36 +0200

    Next-CMS: cleanup

--- apps/next-cms/src/pages/dashboard/components/index.tsx
+++ apps/next-cms/src/pages/dashboard/components/index.tsx
@@ -4,19 +4,21 @@ import { CreateNewComponent, DashboardComponentsProvider, Grid, SearchField } fr
 import type { TNextPageWithLayout } from '~/types';
 
 const DashboardComponentsRoot: TNextPageWithLayout = () => (
-    <DashboardComponentsProvider className="px-4 py-2" mainProps={{ className: 'flex flex-col gap-4' }}>
-        <DashboardTitle>Components</DashboardTitle>
-        <hr />
-        <div className="flex items-center justify-between gap-2">
-            <SearchField />
-            <Grid.SizeToggle />
-        </div>
-        <Grid className="flex-1 overflow-auto" />
-        <aside className="fixed z-10 right-2 bottom-2">
-            <CreateNewComponent.Fab />
-        </aside>
-        <CreateNewComponent />
-    </DashboardComponentsProvider>
+    <main className="flex flex-col gap-4 px-4 py-2">
+        <DashboardComponentsProvider>
+            <DashboardTitle>Components</DashboardTitle>
+            <hr />
+            <div className="flex items-center justify-between gap-2">
+                <SearchField />
+                <Grid.SizeToggle />
+            </div>
+            <Grid className="flex-1 overflow-auto" />
+            <aside className="fixed z-10 right-2 bottom-2">
+                <CreateNewComponent.Fab />
+            </aside>
+            <CreateNewComponent />
+        </DashboardComponentsProvider>
+    </main>
 );
 
 export default DashboardComponentsRoot;
