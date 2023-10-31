abfcbc2e70def515a28ce2a42c508cbb6be0bbbe
Stanimir Stoychev <xstukix@gmail.com>
2023-10-31 12:54:59 +0100

    general cleanup (#4)

--- apps/content-management-system/src/app/dashboard/_components/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './AuthenticatedUserProvider';
-export * from './Header';
-export * from './UserActions';
--- apps/content-management-system/src/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import { withAuthenticatedUserProvider } from '@/app/dashboard/_components';
-
-function Dashboard() {
-    return <>Hello from the Dashboard!</>;
-}
-
-export default withAuthenticatedUserProvider(Dashboard);
