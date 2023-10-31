7cc0bac4cf6645fbdc53382e83876795e70e445f
Stanimir Stoychev <s.stoychev@smartbroker.de>
2023-10-31 10:19:09 +0100

    Next-CMS: fix schemas

--- apps/next-cms/src/app/dashboard/components/_actions/schemas.ts
+++ apps/next-cms/src/app/dashboard/components/_actions/schemas.ts
@@ -3,9 +3,6 @@ import { TPrettify } from '~/types';
 
 // TComponentEntity (domain)
 const zBaseHtmlSchema = z.object({
-    id: z.number(),
-    name: z.string(),
-    description: z.string(),
     htmlProps: z
         .object({
             id: z.string().optional(),
@@ -16,39 +13,43 @@ const zBaseHtmlSchema = z.object({
         .optional(),
 });
 
-type TzComponentEntity = TPrettify<
+type TzBaseHtmlRecursive = TPrettify<
     z.infer<typeof zBaseHtmlSchema> & {
-        html: (string | TPrettify<Pick<TzComponentEntity, 'html' | 'htmlProps'>>)[];
+        html: (string | TPrettify<Pick<TzBaseHtmlRecursive, 'html' | 'htmlProps'>>)[];
     }
 >;
 
-const zPartialBaseHtmlSchema = zBaseHtmlSchema.omit({ id: true, name: true }).partial();
-const zComponentEntity: z.ZodType<TzComponentEntity> = zBaseHtmlSchema.extend({
-    html: z.lazy(() => z.union([z.string(), zComponentEntity]).array()),
+const zBaseHtmlRecursive: z.ZodType<TzBaseHtmlRecursive> = zBaseHtmlSchema.extend({
+    html: z.lazy(() => z.array(z.union([z.string(), zBaseHtmlRecursive]))),
+});
+
+const zPartialBaseHtmlSchema = zBaseHtmlSchema.extend({
+    id: z.number().optional(),
+    name: z.string().optional(),
+    description: z.string().optional(),
+    html: z.lazy(() => z.array(z.union([z.string(), zBaseHtmlRecursive]))),
 });
 
 export type TzNewComponentEntity = TPrettify<
     z.infer<typeof zPartialBaseHtmlSchema> & {
         name: string;
-        html: (string | TPrettify<Pick<TzNewComponentEntity, 'html' | 'htmlProps'>>)[];
     }
 >;
 export const zNewComponentEntity: z.ZodType<TzNewComponentEntity> = zPartialBaseHtmlSchema.extend({
     name: z.string(),
-    html: z.lazy(() => z.union([z.string(), zComponentEntity]).array()),
 });
 
 export type TzUpdateComponentEntity = TPrettify<
     z.infer<typeof zPartialBaseHtmlSchema> & {
         id: number;
         name?: string;
-        html?: (string | TPrettify<Pick<TzComponentEntity, 'html' | 'htmlProps'>>)[];
+        html?: (string | TPrettify<Pick<TzBaseHtmlRecursive, 'html' | 'htmlProps'>>)[];
     }
 >;
 export const zUpdateComponentEntity: z.ZodType<TzUpdateComponentEntity> = zPartialBaseHtmlSchema.extend({
     id: z.number(),
     name: z.string().optional(),
-    html: z.lazy(() => z.union([z.string(), zComponentEntity]).array().optional()),
+    html: z.lazy(() => z.array(z.union([z.string(), zBaseHtmlRecursive]))),
 });
 
 export const zFindComponentQuery = z.object({
