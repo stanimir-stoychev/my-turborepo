c74bbeb970533282892b65cf75e7bd1e4b5a87e0
Stanimir Stoychev <s.stoychev@smartbroker.de>
2023-10-31 12:54:18 +0100

    general cleanup

--- apps/content-management-system/src/app/_components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './AuthenticatedUserActionsToolbar';
-export * from './AwesomeIcon';
-export * from './Menu';
-export * from './PublicLayout';
-export * from './SignInButton';
--- apps/content-management-system/src/app/_context/NextAuthProvider.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { SessionProvider } from 'next-auth/react';
-
-export function NextAuthProvider({ children }: React.PropsWithChildren) {
-    return <SessionProvider>{children}</SessionProvider>;
-}
