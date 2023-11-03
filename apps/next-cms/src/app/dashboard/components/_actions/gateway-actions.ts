d8479bc0df88691e9e0c11b11835bce65d7a8766
Stanimir Stoychev <s.stoychev@smartbroker.de>
2023-11-03 13:09:32 +0100

    Next CMS: cleanup

--- apps/next-cms/src/app/dashboard/components/_actions/gateway-actions.ts
+++ apps/next-cms/src/app/dashboard/components/_actions/gateway-actions.ts
@@ -8,5 +8,3 @@ export const createComponentGateway = ComponentsGateway.create;
 export const readComponentGateway = ComponentsGateway.read;
 export const updateComponentGateway = ComponentsGateway.update;
 export const deleteComponentGateway = ComponentsGateway.delete;
-
-export { isGatewayError } from '~/server/domains/components/gateways/types';
--- apps/next-cms/src/app/dashboard/components/_actions/index.ts
+++ apps/next-cms/src/app/dashboard/components/_actions/index.ts
@@ -1,3 +1,4 @@
 export * from './actions';
 export * from './gateway-actions';
 export * from './schemas';
+export { filterOutGateWayErrors, isGatewayError } from '~/server/domains/components/gateways';
